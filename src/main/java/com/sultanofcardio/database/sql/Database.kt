6a1d0720ba87393febdf59092fb9fce88bca8b1c
sultanofcardio <kenroy.gobourne@gmail.com>
2020-06-12 02:22:05 -0500

    Added javadoc

--- src/main/java/com/sultanofcardio/database/sql/Database.kt
+++ src/main/java/com/sultanofcardio/database/sql/Database.kt
@@ -1,5 +1,10 @@
 package com.sultanofcardio.database.sql
 
+/**
+ * Run all queries safely inside a transaction that will be rolled back if anything goes wrong
+ *
+ * @param work context in which to run queries. Be sure to use the database context provided
+ */
 fun <T> Database.transaction(work: Database.() -> T): T {
     val clone = Database(schema, databaseType, host, port, username, password)
     clone.getConnection().autoCommit = false
